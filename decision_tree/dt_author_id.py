e62bebad680c89d8fc097ed536b856f2253caba9
skl1f <skl1f@ukrgadget.com>
2017-03-31 00:25:39 +0300

    Naive Bayes task completed, pkl files restored and added into .gitattributes

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,16 +12,7 @@ import sys
 from time import time
 from tools.email_preprocess import preprocess
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
-
-
-
-
-#########################################################
-### your code goes here ###
-
-
-#########################################################
--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -13,17 +13,18 @@
 import sys
 from time import time
 from tools.email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf = GaussianNB()
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
 
-
-#########################################################
-### your code goes here ###
-
-
-#########################################################
+accuracy = clf.score(features_test, labels_test)
+print(accuracy)
