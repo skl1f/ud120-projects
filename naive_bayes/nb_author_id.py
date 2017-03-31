ce0d416809c7ad009133ac40cacd67d433c884d2
skl1f <skl1f@ukrgadget.com>
2017-03-31 22:14:40 +0300

    fixed deprecated test split, and completed SVM tasks

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -10,25 +10,24 @@
     Chris has label 1
 """
 
-import sys
 from time import time
 from tools.email_preprocess import preprocess
 from sklearn.naive_bayes import GaussianNB
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
 clf = GaussianNB()
 
 t0 = time()
 clf.fit(features_train, labels_train)
-print("training time: {0}".format(round(time()-t0, 3), "s"))
+print("training time: {0}".format(round(time() - t0, 3), "s"))
 
 t1 = time()
 prediction0 = clf.predict(features_test[1])
-print("prediction time: {0}".format(round(time()-t1, 3), "s"))
+print("prediction time: {0}".format(round(time() - t1, 3), "s"))
 print("prediction: {0}; true result: {1}".format(prediction0, labels_test[1]))
 
 accuracy = clf.score(features_test, labels_test)
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -8,19 +8,29 @@
     Chris has label 1
 """
 
-import sys
 from time import time
 from tools.email_preprocess import preprocess
+from sklearn.svm import SVC
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+# features_train = features_train[:len(features_train) // 100]
+# labels_train = labels_train[:len(labels_train) // 100]
 
+clf = SVC(C=10000.)
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print("training time: {0}".format(round(time() - t0, 3), "s"))
 
-#########################################################
-### your code goes here ###
+t1 = time()
+prediction0 = clf.predict(features_test)
+print("prediction time: {0}".format(round(time() - t1, 3), "s"))
+# print("10: {0}, 26: {1}, 50: {2}".format(prediction0[10], prediction0[26], prediction0[50]))
+print(sum(prediction0))
 
-#########################################################
+accuracy = clf.score(features_test, labels_test)
+print("Accuracy {0}".format(accuracy))
